09cd03055fdc56df7739e0396aea93335b69d7ed
BlueRGB <38169403+BlueRGB@users.noreply.github.com>
2018-11-16 18:28:28 -0500

    changes to JMorph

--- src/src/JMorph.java
+++ src/src/JMorph.java
@@ -1,3 +1,114 @@
-public class JMorph {
-    //This is a test
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class JMorph extends JFrame {
+
+    private JPanel panel, controls, images, leftPanel, rightPanel;
+    private JButton uploadLeft, uploadRight, quit;
+    private BufferedImage leftImage, rightImage;
+
+
+
+
+    public JMorph(){
+        final JFileChooser fc = new JFileChooser(".");
+        panel  = new JPanel();
+        images = new JPanel();
+        leftPanel = new JPanel();
+        rightPanel = new JPanel();
+        controls = new JPanel();
+        uploadLeft = new JButton("Upload Left Image");
+        uploadRight = new JButton("Upload Right Image");
+        quit = new JButton("Quit");
+
+        leftPanel.setPreferredSize(new Dimension(200, 200));
+        rightPanel.setPreferredSize(new Dimension(200, 200));
+        images.add(leftPanel, BorderLayout.WEST);
+        images.add(rightPanel, BorderLayout.EAST);
+        panel.setLayout(new GridLayout(2,1,5,5));
+
+        quit.addActionListener(e -> System.exit(0));
+        uploadRight.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed (ActionEvent e) {
+                        int returnVal = fc.showOpenDialog(null);
+                        if (returnVal == JFileChooser.APPROVE_OPTION) {
+                            File file = fc.getSelectedFile();
+                            String name = file.getAbsolutePath();
+                            try {
+                                rightImage = ImageIO.read(new File(name));
+                            } catch (IOException e1){};
+
+                            rightImage = resize(rightImage, 200, 200);
+                        }
+                    }
+                }
+        );
+        uploadLeft.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed (ActionEvent e) {
+                        int returnVal = fc.showOpenDialog(null);
+                        if (returnVal == JFileChooser.APPROVE_OPTION) {
+                            File file = fc.getSelectedFile();
+                            String name = file.getAbsolutePath();
+                            try {
+                                leftImage = ImageIO.read(new File(name));
+                            } catch (IOException e1){};
+
+                            leftImage = resize(leftImage, 200, 200);
+                        }
+                    }
+                }
+        );
+        controls.add(uploadRight);
+        controls.add(uploadLeft);
+        controls.add(quit);
+        panel.add(images);
+        panel.add(controls);
+        add(panel);
+
+
+
+
+
+
+        pack();
+        addWindowListener(new WindowAdapter()
+        {
+            public void windowClosing(WindowEvent e)
+            {
+                System.exit(0);
+            }
+        });
+        setResizable(false);
+        setVisible(true);
+
+
+    }
+
+    public BufferedImage resize(BufferedImage image, int newWidth, int newHeight) {
+        Image temp = image.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        BufferedImage newImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = newImage.createGraphics();
+        g2d.drawImage(temp, 0, 0, null);
+        g2d.dispose();
+
+        return newImage;
+    }
+
+    public static void main(String argv[]){
+        JMorph please = new JMorph();
+    }
+
+
 }
