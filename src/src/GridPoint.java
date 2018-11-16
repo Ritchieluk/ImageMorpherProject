9cd10e1c947d43ee558654ae99af5d6f73d0899b
Ritchieluk <33823229+Ritchieluk@users.noreply.github.com>
2018-11-15 22:00:35 -0500

    Gridpoint started

--- src/src/GridPoint.java
+++ src/src/GridPoint.java
@@ -1,2 +1,31 @@
-public class GridPoint {
+import javafx.scene.shape.Circle;
+
+import javax.swing.*;
+
+public class GridPoint extends JComponent{
+
+    int xValue, yValue, startY, startX;
+    int selected = -1, radius = 5;
+    boolean running = false;
+    Circle point;
+
+    public GridPoint(int x, int y){
+        xValue = x;
+        yValue = y;
+        point = new Circle(x, y, radius);
+    }
+
+    public void startDrag(int x, int y){
+
+    }
+
+    public void doDrag(int x, int y){
+        if(selected >-1){
+            xValue = x;
+        }
+    }
+
+    public void setxValue(int newX){
+        xValue = newX;
+    }
 }
