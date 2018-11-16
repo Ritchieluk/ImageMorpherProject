c79e3b46edbaffbc36a04037c5a827221c68e746
Ritchieluk <33823229+Ritchieluk@users.noreply.github.com>
2018-11-15 22:17:38 -0500

    Image Grid started

--- src/src/ImageGrid.java
+++ src/src/ImageGrid.java
@@ -1,2 +1,17 @@
-public class ImageGrid {
+import javax.swing.*;
+
+public class ImageGrid extends JPanel {
+    int width, height;
+    GridPoint[][] points;
+
+    public ImageGrid(int w, int h){
+        width = w;
+        height = h;
+
+        for(int i = 0; i < w; i++){
+            for(int j = 0; j<h; j++){
+                points[i][j] = new GridPoint(i*20, j*20);
+            }
+        }
+    }
 }
